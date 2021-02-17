9e031389d60dae76dd7597cf03d0bd1f1012474c
Harrshini B <72803388+harrshini12@users.noreply.github.com>
2021-02-17 14:58:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/Public'))
 
 app.listen(process.env.PORT || 3000, 
 () => console.log("Server is running..."));
